0b306482b8b99b6b8c86253ae18a9cad1e82d493
Christopher D. Miller <millerc8@allegheny.edu>
2019-01-17 14:09:15 -0500

    added sessions

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, session,url_for,redirect
 from flask import request
 from timeLogger import TimeLogger
 from datetime import date
 import datetime
 
 app = Flask(__name__)
+app.secret_key = 'hello'
 
-x=TimeLogger()
+seleniumSesssion={}
 
 
 @app.route("/home", methods=['GET',])
@@ -14,20 +15,25 @@ def home():
     return render_template("login.html")
 @app.route("/login",methods=['POST'])
 def login():
-    x.user = request.form['user']
-    x.passw = request.form['pass']
-    x.webLogin()
-    jobs = x.showJobs()
+    session["user"] = request.form['user']
+    seleniumSesssion[session["user"]] = TimeLogger()
+    print(len(seleniumSesssion))
+    seleniumSesssion[session["user"]].user = session["user"]
+    seleniumSesssion[session["user"]].passw = request.form['pass']
+
+    seleniumSesssion[session["user"]].webLogin()
+    jobs = seleniumSesssion[session["user"]].showJobs()
     return render_template("submitHours.html",jobs = jobs)
 @app.route("/done", methods=['POST'])
 def done():
-    
-    x.selectJob(int(request.form['workplace'])+1)
+
+    seleniumSesssion[session["user"]].selectJob(int(request.form['workplace'])+1)
     date = request.form["day"].split("-")
     dateP = datetime.date(int(date[0]),int(date[1]),int(date[2]))
-    x.selectMonth(dateP.month)
-    x.goToDay(dateP)
-    x.enterHours(request.form['hours'])
-    x.close()
-    x.__init__()
+    seleniumSesssion[session["user"]].selectMonth(dateP.month)
+    seleniumSesssion[session["user"]].goToDay(dateP)
+    seleniumSesssion[session["user"]].enterHours(request.form['hours'])
+    seleniumSesssion[session["user"]].close()
+    session.pop('user', None)
+    return redirect(url_for('home'))
     return render_template("login.html")
