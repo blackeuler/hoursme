6dfa0ece0e6b71f8b1d6f882ea32cf7a00274a58
Christopher Miller <millerc8@allegheny.edu>
2019-01-23 03:50:55 -0500

    Added more error handling

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask,render_template, request, session,url_for,redirect
-from flask import request
 from timeLogger import TimeLogger
 from datetime import date
 import datetime
 
 app = Flask(__name__)
-app.secret_key = 'hello'
+app.secret_key = 'hellogfd'
 
 seleniumSesssion={}
 
@@ -18,7 +17,11 @@ def home(errors=""):
     return render_template("login.html", errors = errors, messages = messages)
 @app.route("/login",methods=['POST'])
 def login():
+    print(f"Selenium Session@login1:{seleniumSesssion}")
     session["user"] = request.form['user']
+    if session["user"] in seleniumSesssion:
+        session["errors"] = ["You are already Logged in, Can only login on one device"]
+        return redirect(url_for('home',errors=session["errors"],user =session["user"]))
     seleniumSesssion[session["user"]] = TimeLogger()
     seleniumSesssion[session["user"]].user = session["user"]
     seleniumSesssion[session["user"]].passw = request.form['pass']
@@ -26,11 +29,15 @@ def login():
     loginSuccess = seleniumSesssion[session["user"]].webLogin()
     if loginSuccess:    
         jobs = seleniumSesssion[session["user"]].showJobs()
-        return render_template("submitHours.html",jobs = jobs)
+        print(f"Selenium Session@login2:{seleniumSesssion}")
+        return render_template("submitHours.html",jobs = jobs,user =session["user"])
     session["errors"] = ["Wrong Password Please try again"]
+    seleniumSesssion.pop(session["user"],None)
+    session.pop('user', None)
     return redirect(url_for('home',errors=session["errors"]))
 @app.route("/done", methods=['POST'])
 def done():
+    print(f"SeleniumSession@Done trying to submit{seleniumSesssion}")
     try:
         seleniumSesssion[session["user"]].selectJob(int(request.form['workplace'])+1)
     except KeyError:
@@ -43,8 +50,10 @@ def done():
         seleniumSesssion[session["user"]].goToDay(dateP)
         seleniumSesssion[session["user"]].enterHours(request.form['hours'])
         seleniumSesssion[session["user"]].close()
+        seleniumSesssion.pop(session["user"],None)
         session.pop('user', None)
         return redirect(url_for('home',messages="hours logged succesfully"))
     except:
         print("something went wrong")
+        seleniumSesssion.pop(session["user"],None)
         return redirect(url_for('home',errors=['Did not find TimeSheet']))
